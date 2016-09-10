3d559fe91e76aae18d7df6301e4f7f6086684108
cleissont <cleissontrindade@hotmail.com>
2016-09-10 17:16:51 -0300

    inserindo dados

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CarController extends Controller
+{
+	public function saveCar(Request $request)
+	{ 
+		var_dump($request);
+
+		# code...
+	}
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastros', function () {
+    return view('registercar');
+});
+
+Route::post('/cadastros', 'CarController@saveCar');
